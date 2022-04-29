452f3ad27d16194fe891237fe57b148b2a84c945
Micael Quilião Gerson <micaelquiliao20@gmail.com>
2022-04-29 12:09:12 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MySqlTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(187, 38);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(11, 12);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(123, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(140, 12);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(284, 20);
+            this.txtSobrenome.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(268, 38);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(349, 38);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 71);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySqlTest
+{
+    public partial class Form1 : Form
+    {
+
+        private MySqlConnection bdConn; //MySQL
+        private MySqlDataAdapter bdAdapter;
+        private DataSet bdDataSet; //MySQL
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MySqlConnection(string query)
+        {
+            //Definição do dataset
+            bdDataSet = new DataSet();
+
+            //Define string de conexão
+            bdConn = new MySqlConnection("            server=localhost;" +
+                "                                       database=teste;" +
+                "                                             uid=xxxxx;" +
+                "                                     pwd=xxxxxxxxxxxxx");
+
+            //Abre conexão
+            try
+            {
+                bdConn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Impossível estabelecer conexão" + ex.Message);
+            }
+            //Verifica se a conexão está aberta
+            if (bdConn.State == ConnectionState.Open)
+            {
+                //Se estiver aberta insere os dados na BD
+                MySqlCommand commS = new MySqlCommand(query, bdConn);
+                commS.ExecuteNonQuery();
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string query = $"INSERT INTO TESTEE (NOME, SOBRENOME) VALUES ('{txtNome.Text}','{txtSobrenome.Text}')";
+
+            //Executa a query 
+            MySqlConnection(query);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = $"UPDATE TESTEE SET SOBRENOME = '{txtSobrenome.Text}' WHERE NOME = '{txtNome.Text}'";
+
+            //Executa a query 
+            MySqlConnection(query);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = $"DELETE FROM TESTEE WHERE NOME ='{txtNome.Text}' AND SOBRENOME = '{txtSobrenome.Text}'";
+
+            //Executa a query 
+            MySqlConnection(query);
+        }
+    }
+}
